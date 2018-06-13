504c7f07e01c2d92003a3c3e14f0a4f3219ff72d
Anthony COLAS <anthonycolas.france@gmail.com>
2018-06-13 17:06:35 +0200

    Mobile Android - OK

--- Assets/Scripts/ClickMouse.cs
+++ Assets/Scripts/ClickMouse.cs
@@ -9,6 +9,7 @@ public class ClickMouse : MonoBehaviour
     private int     CntZeros;
     private string  strZeros;
     private Vector3 v3Pos;
+    private Vector2 v2Pos;
     private int     threshold = 9;
 
     private void Awake()
@@ -21,6 +22,7 @@ public class ClickMouse : MonoBehaviour
 
     public void Update()
     {
+        #if UNITY_EDITOR
         if (Input.GetButtonDown("Fire1"))
         {
             modeOn = true;
@@ -102,4 +104,89 @@ public class ClickMouse : MonoBehaviour
             }
         v3Pos = Input.mousePosition;
      }
+
+     #else
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            modeOn = true;
+        }
+        if (modeOn)
+        {
+            OnTouchDrag();
+            if (currentPosition == 101)
+            {
+                currentPosition = 0; 
+            }
+            else if (currentPosition == 0)
+            {
+                currentPosition = 100; 
+            }
+            ImageRefresh();
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            modeOn = false;
+        }
+    }
+    void    RefreshZeros(int currentPosition)
+    {
+        if (currentPosition > 10000)
+            Debug.Log("Log Error");
+        else if (currentPosition < 10) 
+        {
+            CntZeros = 3;
+        }
+        else if (currentPosition < 100)
+            CntZeros = 2; 
+        else if (currentPosition == 100)
+        {
+            CntZeros = 1; 
+        }
+    }
+
+    void    ImageRefresh()
+    {
+       RefreshZeros(currentPosition);
+                    if (CntZeros == 3)
+                        strZeros = "000";
+                    else if (CntZeros == 2)
+                        strZeros = "00";
+                    else if (CntZeros == 1)
+                        strZeros = "0";
+                    else if (CntZeros == 0)
+                        strZeros = "";
+                    GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/Cochenec_rendu_anim" + strZeros + currentPosition.ToString());
+    }
+
+        void OnTouchDrag() 
+        {
+        var v2 = Input.GetTouch(0).position - v2Pos;
+        v2.Normalize();
+        var f = Vector2.Dot(v2, Vector2.up);
+        if (Vector2.Distance(v2Pos, Input.GetTouch(0).position) < threshold) {
+             Debug.Log("No movement");
+        return;
+        }
+ 
+        if (f >= 0.5) {
+            Debug.Log("Up");
+        }
+        else if (f <= -0.5) {
+            Debug.Log("Down");
+        }
+        else {
+            f = Vector2.Dot(v2, Vector2.right);
+            if (f >= 0.5) {
+                Debug.Log("Right");
+                currentPosition--;
+            }
+            else {
+                 Debug.Log("Left");
+                 currentPosition++;
+                }
+            }
+        v2Pos = Input.GetTouch(0).position;
+     }
+
+     #endif
 }
